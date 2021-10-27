7e4c703bedd7265cac29bbefaa947cd44f3d83b7
Marcus Rothoff <marcusrothoff@hotmail.com>
2021-10-27 21:54:40 +0200

    Add welcoming message to new players with name input

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+
+## A welcoming message and invite to play
+print("\n Oh, no, no, no, not you again... \n")
+time.sleep(1)
+print("\nRemind me, I seem to have intentionally misplaced your name. Do remind me, so I can forget again.\n")
+name = input("Enter your forgetful name:\n")
+time.sleep(2)
+print("\nAnd here I thought " + name + " wasn't up for the challenge of facing me!\n")
+time.sleep(2)
+print("\nWell, Harold...Harold, was it? " + name + " , I know, I know. I've considered it and will allow you to play the game.\n")
