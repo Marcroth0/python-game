2644e7f755e459c9bf512fae85d48e50603c020f
Marcus Rothoff <marcusrothoff@hotmail.com>
2021-10-31 21:23:45 +0100

    try with class

--- run.py
+++ run.py
@@ -1,119 +1,51 @@
 import random
-import time
+wordsL1 = ["pasta", "spaghetti", "maccheroni"]
 
-wordsL1 = ['DRAGON', 'GOLDEN', 'TREASURE', 'DIAMOND', 'MOUNTAIN', 'COINS', 'ENGULFED', 'DESTRUCTION', 'DEMOLISHED']
-wrong_guesses=0
-global lives
-guessed_char = []
-attempts = []
+class HangmanGame: 
+    def __init__(self, wordsList, lives):
+        self.wordsToPlay = wordsList
+        self.plain_text = random.choice(wordsList)
+        self.hidden = ("_" * len(self.plain_text))
+        self.lives = lives
 
-## A welcoming message and invite to play
+    def updateHidden(self, index):
+        self.hidden = self.hidden[:index] + self.plain_text[index] + self.hidden[index+1:]
+
 def main():
-    # print("\n Oh, no, no, no, not you again... \n")
-    # time.sleep(1)
-    # print("\nRemind me, I seem to have intentionally misplaced your name. Do remind me, so I can forget again.\n")
-    # name = input("Enter your forgetful name:\n")
-    # time.sleep(1)
-    # print("\nAnd here I thought " + name + " wasn't up for the challenge of facing me!\n")
-    # time.sleep(1)
-    # print("\nWell, Harold...Harold, was it? " + name + " , I know, I know. I've considered it and will allow you to play the game.\n")
-    # print("\nBut before we start...")
-    # time.sleep(1)
-    # print("\n" + name + " Haroldsen, how about before I devour you with the shere sight of me, we start off with a riddle?\n")
-    # start_game = input("Start game y/n: ")
-    # if start_game == 'y':
-    #     startgame()
-    # else:
-    #     print('Bye Felicia')
-    #     exit(0)
-    myword = random_words(wordsL1) 
-    lives = 5
-    
+    ## Check if they actually want to play, if so:
+    game = HangmanGame(wordsL1, 5)
+    tries = []
+    print(game.plain_text)
+    print(game.hidden)
+    while(True):
+        guess = input("Please enter a guess, it must be a letter!, go ahead: ")
+        matchedIndexes = (checkMatch(game, guess))
+        if len(matchedIndexes):
+            updateDisplayedWord(game, matchedIndexes)
+        else:
+            tries.append(guess)
+            game.lives -= 1
+        
+        winOrLose(game)
+        print(game.hidden)
+        print("Tried letters: ")
+        print(tries)
+        print(f"Lives: {game.lives}")
 
-    while wrong_guesses < lives:
-        #print(hangman_lives[lives])
-        GUESS = input("Oh, do try me with a letter: ")
-        charMatch(hidden_word, GUESS.upper(), lives)
+def checkMatch(game, guess):
+    return [i for i, ltr in enumerate(game.plain_text) if ltr == guess]
 
-## Check if the char matches
-def charMatch(word, guess, lives):
+def updateDisplayedWord(game, list):
+    print(list)
+    for idx in list:
+        game.updateHidden(idx)
 
-
-
-
-
-
-
-## Start the game
-
-## Random words
-def random_words(list_words):
-    random.shuffle(list_words)
-    return list_words[0]
-
-## Play again
-def play_again():
-    replay_question = input("How about another game? Enter 'Y' for yes or 'N' for no.")
-    if replay_question == 'y':
-        startgame()
-    else:
-        print("Perhaps another time.")
-
-
-def hangman_lives(lives):
-    
-    hangman_images =  ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
-
-    return hangman_images[lives]
-
+def winOrLose(game):
+    if (game.hidden == game.plain_text):
+        print("Congrats, you won!")
+        exit(0)
+    elif(game.lives == 0):
+        print("You lost, poor idiot")
+        exit(0)
 
 main()
