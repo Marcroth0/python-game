9306c8a19ec31bf01da53a27cc4df1e977870979
Marcus Rothoff <marcusrothoff@hotmail.com>
2021-11-01 22:44:05 +0100

    add another difficulty

--- run.py
+++ run.py
@@ -1,32 +1,44 @@
 import random
-wordsL1 = ["pasta", "spaghetti", "maccheroni"]
-wordsL2 = ["pasteroni", "maccharetti", "cannelloncelli"]
+import time
+wordsL1 = ["dragon", "engulfed", "diamonds", "jewels", "treasure", "destruction"]
+wordsL2 = ["phenomenon", "onomatopoeia", "disinterested", "irregardless"]
 
 class HangmanGame: 
-    def __init__(self, wordsList, lives):
+    def __init__(self, wordsList, imageArray):
         self.wordsToPlay = wordsList
         self.plain_text = random.choice(wordsList)
         self.hidden = ("_" * len(self.plain_text))
-        self.lives = lives
         self.tries = []
+        self.displayedImage = imageArray
+        self.lives = len(self.displayedImage) -1
 
     def updateHidden(self, index):
         self.hidden = self.hidden[:index] + self.plain_text[index] + self.hidden[index+1:]
 
 def main():
+    choice = input("Easy or hard? Enter 'easy' for Easy, and 'hard' for Hard: ").upper()
     ## Check if they actually want to play, if so:
-    startGame(wordsL1, 7)
-    
+    if choice == 'EASY':
+        startGame(wordsL1, HANGMANPICS_EASY)
+    elif choice == 'HARD':
+        startGame(wordsL2, HANGMANPICS_HARD)
+
 
 def startGame(listOfWords, lives):
     game = HangmanGame(listOfWords, lives)
     print("----------- THE GAME BEGINS----------")
     print("\n")
-    print(game.hidden)
-    print("\n")
-    print(f"Lives: {game.lives}")
-    print("\n")
+    
     while(True):
+        print(game.displayedImage[-game.lives-1])
+        print("\n")
+        print(game.hidden)
+        print("\n")
+        print("Tried letters/words: ")
+        print(*game.tries, sep = ", ")
+        print("\n")
+        print(f"Lives: {game.lives}")
+        print("\n")
         guess = input("Please enter a guess, it must be a letter!, go ahead: ")
         if validateInput(game, guess):
             matchedIndexes = (checkMatch(game, guess))
@@ -34,18 +46,15 @@ def startGame(listOfWords, lives):
                 updateDisplayedWord(game, matchedIndexes)
             else:
                 game.tries.append(guess)
-                print(HANGMANPICS[-game.lives])
                 game.lives -= 1
                 
             
             winOrLose(game)
-            print("\n")
-            print(game.hidden)
-            print("\n")
-            print("Tried letters/words: ")
-            print(*game.tries, sep = ", ")
-            print("\n")
-            print(f"Lives: {game.lives}")
+            # print("\n")
+            # print(game.hidden)
+            # print("\n")
+            # print("\n")
+            # print(f"Lives: {game.lives}")
 
 def checkMatch(game, guess):
     return [i for i, ltr in enumerate(game.plain_text) if ltr == guess]
@@ -59,10 +68,14 @@ def winOrLose(game):
         print(f"Congrats, you won! The word was {game.plain_text}")
         playAgain = input("Wanna go ahead to the next level(y/n): ")
         if playAgain == "y":
-            startGame(wordsL2, 3)
+            main()
         exit(0)
     elif(game.lives == 0):
-        print("You lost, poor idiot")
+        print(game.displayedImage[-1])
+        print(f"You lost, poor idiot. The word was {game.plain_text}")
+        playAgain = input("Wanna go ahead to the next level(y/n): ")
+        if playAgain == "y":
+            main()
         exit(0)
 
 def validateInput(game, guess):
@@ -82,6 +95,7 @@ def validateInput(game, guess):
 def checkMatchWord(game, guess):
     if (guess == game.plain_text):
         print(f"Congrats, you won! The word was {game.plain_text}")
+        
         exit(0)
     else:
         print("You guessed the wrong word! Try again")
@@ -89,56 +103,151 @@ def checkMatchWord(game, guess):
         game.tries.append(guess)
         print(game.lives)
 
-HANGMANPICS = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
+HANGMANPICS_EASY = ['''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,            ,|
+      | )          ( |
+      |/            \|
+      (_            _)
+       \__        __/
+        |          |
+        \          /
+         `--------`
+''', '''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,       .-.  ,|
+      | )      \__)( |
+      |/            \|
+      (_            _)
+       \__        __/
+        |          |
+        \          /
+         `--------`
+''', '''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,  .-.  .-.  ,|
+      | )(__/  \__)( |
+      |/            \|
+      (_            _)
+       \__        __/
+        |          |
+        \          /
+         `--------`
+''', '''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,  .-.  .-.  ,|
+      | )(__/  \__)( |
+      |/     /\     \|
+      (_     ^^     _)
+       \__        __/
+        |          |
+        \          /
+         `--------`
+''', '''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,  .-.  .-.  ,|
+      | )(__/  \__)( |
+      |/     /\     \|
+      (_     ^^     _)
+       \__        __/
+        | \IIIIII/ |
+        \          /
+         `--------`
+''', '''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,  .-.  .-.  ,|
+      | )(__/  \__)( |
+      |/     /\     \|
+      (_     ^^     _)
+       \__|IIIIII|__/
+        | \IIIIII/ |
+        \          /
+         `--------`
+''']
 
+HANGMANPICS_HARD = ['''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,            ,|
+      | )          ( |
+      |/            \|
+      (_            _)
+       \__        __/
+        |          |
+        \          /
+         `--------`
+''', '''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,  .-.  .-.  ,|
+      | )(__/  \__)( |
+      |/            \|
+      (_            _)
+       \__        __/
+        |          |
+        \          /
+         `--------`
+''','''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,  .-.  .-.  ,|
+      | )(__/  \__)( |
+      |/            \|
+      (_     /\     _)
+       \__        __/
+        |          |
+        \          /
+         `--------`
+''', '''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,  .-.  .-.  ,|
+      | )(__/  \__)( |
+      |/            \|
+      (_     ^^     _)
+       \__        __/
+        |          |
+        \          /
+         `--------`
+''', '''
+           ______
+        .-"      "-.
+      //            \\
+      |              |
+      |,  .-.  .-.  ,|
+      | )(__/  \__)( |
+      |/     /\     \|
+      (_     ^^     _)
+       \__|IIIIII|__/
+        | \IIIIII/ |
+        \          /
+         `--------`
+''']
 main()
 
