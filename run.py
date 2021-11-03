a7b9d6e9693da412c825c46d0699054c4de6ceb9
Marcus Rothoff <marcusrothoff@hotmail.com>
2021-11-03 19:49:55 +0100

    add name to playagain and removed unnecessary code

--- run.py
+++ run.py
@@ -25,14 +25,14 @@ def main():
     print("Many have come, none have left. What makes you think you will be the first to defeat the Skull of Kol'dan?")
     time.sleep(1)
     print("Centuries upon centuries I've feasted on the souls of these trials. Tell me, what is your name?")
-
+    name = input("Enter your name: ")
+    time.sleep(1)
+    print("Oh, another " + name +  " have already perished upon the sight of me. Will you be the second?")
+    time.sleep(1)
 
 
     while True:
-        name = input("Enter your name: ")
-        time.sleep(1)
-        print("Oh, another " + name + " have already perished upon the sight of me. Will you be the second?")
-        time.sleep(1)
+        
         choice = input("Easy or hard? Enter 'easy' for Easy, and 'hard' for Hard: ").upper()
         #Check if they actually want to play, if so:
         if choice == 'EASY':
@@ -40,6 +40,7 @@ def main():
         elif choice == 'HARD':
             startGame(wordsL2, HANGMANPICS_HARD, name)
         elif choice == 'EXIT':
+            print(f"Sad to see you go.")
             exit(0)
         else:
             print("Please enter (easy/hard) or (exit) to quit")
@@ -48,18 +49,9 @@ def main():
 def startGame(listOfWords, lives, name):
     game = HangmanGame(listOfWords, lives, name)
     print("----------- THE GAME BEGINS----------")
-    print("\n")
+    displayInfo(game)
     
     while(game.lives > 0 and game.hidden != game.plain_text):
-        print(game.displayedImage[-game.lives-1])
-        print("\n")
-        print(game.hidden)
-        print("\n")
-        print("Tried letters/words: ")
-        print(*game.tries, sep = ", ")
-        print("\n")
-        print(f"Lives: {game.lives}")
-        print("\n")
         guess = input("Please enter a guess, it must be a letter!, go ahead: ").upper()
         if validateInput(game, guess):
             matchedIndexes = (checkMatch(game, guess))
@@ -68,70 +60,75 @@ def startGame(listOfWords, lives, name):
             else:
                 game.tries.append(guess)
                 game.lives -= 1
-                print("Hah! " + game.name + " Look at you trying your best!")
-                
+                print(f"Hah! {game.name}, Look at you trying your best!")    
+            displayInfo(game)
             winOrLose(game)
-            # print("\n")
-            # print(game.hidden)
-            # print("\n")
-            # print("\n")
-            # print(f"Lives: {game.lives}")
-
+            
 
 def checkMatch(game, guess):
     return [i for i, ltr in enumerate(game.plain_text) if ltr == guess]
-
 
 def updateDisplayedWord(game, list):
     for idx in list:
         game.updateHidden(idx)
 
-
-def playAgain():
+def playAgain(game):
     playAgain_question = input("Wanna play again? (y/n): ").upper()
     if playAgain_question == "Y":
         return
     elif playAgain_question == "N":
+        print(f"Sad to see you go, {game.name}. All things must come to an end.")
         exit(0)
-
 
 def winOrLose(game):
     if (game.hidden == game.plain_text):
         print(f"Congrats, you won! The word was {game.plain_text}")
-        playAgain()
+        print("I guess there had to be a first...")
+        playAgain(game)
     elif(game.lives == 0):
         print(game.displayedImage[-1])
-        print(f"You lost, poor idiot. The word was {game.plain_text}")
-        playAgain()
-
+        print(f"You lost, {game.name}! The word was {game.plain_text}")
+        print(f"Another one to add to my collection!")
+        playAgain(game)
 
 def validateInput(game, guess):
     if not guess.isalpha():
         print("Please enter a letter [a, b, c ..]: ")
         return False
-    elif guess == "exit":
+    elif guess == "EXIT":
+        print("Sad to see you go.")
         exit(0)
     elif len(guess) > 1:
-        answer = input("Are you sure you want to guess the whole word? It's gonna cost you lives! (y/n): ")
+        answer = input(f"Are you sure you want to guess the whole word, {game.name}? It's gonna cost you lives! (y/n): ")
         return False if answer == "n" else checkMatchWord(game, guess)
     elif guess in game.tries or guess in game.hidden:
-        print("You already tried that letter, " + game.name)
+        print(f"You already tried that letter, {game.name}")
         return False
     else:
         return True
-
 
 def checkMatchWord(game, guess):
     if (guess == game.plain_text):
         game.hidden = game.plain_text
         print(f"Congrats, you won! The word was {game.plain_text}")
-        playAgain()
+        playAgain(game)
     else:
         print("You guessed the wrong word! Try again")
         game.lives -= 1
         game.tries.append(guess)
         print(game.lives)
 
+def displayInfo(game):
+    print("\n")
+    print(game.displayedImage[-game.lives-1])
+    print("\n")
+    print(game.hidden)
+    print("\n")
+    print("Tried letters/words: ")
+    print(*game.tries, sep = ", ")
+    print("\n")
+    print(f"Lives: {game.lives}")
+    print("\n")
 
 HANGMANPICS_EASY = ['''
            ______
